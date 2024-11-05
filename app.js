41f1210b1fdf3e0ba3186dcf0e2c0f77d45755c9
emanuele23dev <emanuele.meggiarin@libero.it>
2024-11-05 15:56:30 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const host = "http://127.0.0.1";
+
+
+app.listen(port, () => {
+  console.log(`App listening on ${host}:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Express Blog Api Crud");
+});
